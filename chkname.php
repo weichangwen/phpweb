ae8d9513d1b48e16630a6995e728ffa0d5ac4c10
wcw <wcw@qq.com>
2017-09-08 17:08:19 +0800

    123

--- chkname.php
+++ chkname.php
@@ -1,5 +1,9 @@
 <?php
 	session_start();
 	header("Content-type:text/html;charset=UTF-8;");
+	require("system/system.inc.php");
+	$reback='0';
+	$sql = "select * from tb_user where name='".$_GET['user']"'";
+	
 
 ?>
